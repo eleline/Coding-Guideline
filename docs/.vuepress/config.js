a383e1296729211a63e0bdb884a0006805f379f3
eleline <ggggepicstar@gmail.com>
2019-12-15 10:58:34 +0900

    config の重複を削除

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,7 +28,6 @@ module.exports = {
   plugins: [
     "reading-progress",
     "smooth-scroll",
-    ["container", { type: "right", dafaultTitle: "" }],
     ['container', {
       type: 'right',
       defaultTitle: '',
